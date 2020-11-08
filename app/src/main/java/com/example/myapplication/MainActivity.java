3d66983bd4a740f77319c75b0603207aa1e06d06
Abdelkhalek Guedri <abdelkalek@gmail.com>
2020-11-08 18:05:30 +0100

    Mega function and Dialog commit
    AlertDialog and mega function commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -31,53 +34,57 @@ CheckBox chechB ;
         chechB =(CheckBox)findViewById((R.id.CheckB_Mega));
         resu.setText("");
     }
+    public void calcule(double Resultat)
+    {
+        DecimalFormat df = new DecimalFormat("#.##");
+        String compar="";
+        if (Resultat<18.5) { compar=" Insuffisance pondérale (maigreur)"; }
+        else if(Resultat>18.5&&Resultat<25) { compar="Corpulence normale "; }
+        else if(Resultat>25&&Resultat<30){compar="Surpoids";}
+        else if(Resultat>30&&Resultat<35){compar="Obésité modérée";}
+        else if(Resultat>35&&Resultat<40){compar="Obésité sévère";}
+        else if(Resultat>40){compar="Obésité morbide ou massive";}
+       if(chechB.isChecked()==false){compar="";}
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("Resultat de IMC");
+        dialog.setMessage(String.valueOf(df.format(Resultat))+"\n"+compar);
+        dialog.setCancelable(true);
+        dialog.setNegativeButton(
+                "Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert = dialog.create();
+        alert.show();
+
+     //   resu.setText(String.valueOf(df.format(Resultat))+"\n"+compar);
+    //    resu.setTextColor(Color.parseColor("#03A9F4"));
+        //    ((Button)v).setText("Ok");
+    }
+
     public void onClickBnt(View v )
     {
 if(poids.getText().length()!=0&&taille.getText().length()!=0){
         Double poid = Double.parseDouble(poids.getText().toString());
     Double tail = Double.parseDouble(taille.getText().toString());
     Double Resultat ;
-    DecimalFormat df = new DecimalFormat("#.##");
-    String compar="";
-
 
 
         if (R1.isChecked()==true)
         {
             Resultat = poid/(tail*tail);
             Button button=(Button) v;
+            calcule(Resultat);
 
-            if (Resultat<18.5)
-            {
-                compar=" Insuffisance pondérale (maigreur)";
-            }else if(Resultat>18.5&&Resultat<25)
-            {
-                compar="Corpulence normale ";
-            }else if(Resultat>25&&Resultat<30){compar="Surpoids";}
-            else if(Resultat>30&&Resultat<35){compar="Obésité modérée";}
-            else if(Resultat>35&&Resultat<40){compar="Obésité sévère";}
-            else if(Resultat>40){compar="Obésité morbide ou massive";}
-            resu.setText(String.valueOf(df.format(Resultat))+"\n"+compar);
-            resu.setTextColor(Color.parseColor("#03A9F4"));
-            //    ((Button)v).setText("Ok");
         }else if(R2.isChecked()==true)
         {
             Resultat = poid/((tail/100)*(tail/100));
+            calcule(Resultat);
 
-            if (Resultat<18.5)
-            {
-                compar=" Insuffisance pondérale (maigreur)";
-            }else if(Resultat>18.5&&Resultat<25)
-            {
-                compar="Corpulence normale ";
-            }else if(Resultat>25&&Resultat<30){compar="Surpoids";}
-            else if(Resultat>30&&Resultat<35){compar="Obésité modérée";}
-            else if(Resultat>35&&Resultat<40){compar="Obésité sévère";}
-            else if(Resultat>40){compar="Obésité morbide ou massive";}
-            resu.setText(String.valueOf(df.format(Resultat))+"\n"+compar);
-            resu.setTextColor(Color.parseColor("#03A9F4"));
-            Button button=(Button) v;
-       //     ((Button)v).setText("Abdelkalek");
         }
 }
 else
@@ -92,6 +99,8 @@ else
         poids.setText("");
         taille.setText("");
         resu.setText("");
+
+
     }
     
 }
